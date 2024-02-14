c91918aa17cca805486bf5e0d1cc8f72d6fc6d1d
김다인 <dbslzhs1218@naver.com>
2024-02-14 23:01:57 +0900

    feat: 240214_Lv0_몫 구하기

---
+++ day1/Lv0_몫구하기.ts
@@ -0,0 +1,8 @@
+function quotient(num1: number, num2: number): number {
+    let answer = Math.trunc(num1 / num2);
+    
+    return answer;
+}
+
+console.log(quotient(32, 7));
+console.log(quotient(12, 5));
