58448cacc6fd88e2d852ca6f6b48b672210ad237
김다인 <dbslzhs1218@naver.com>
2024-02-15 02:53:39 +0900

    feat: 240215_Lv1_약수의 합

---
+++ day1/Lv1_약수의합.ts
@@ -0,0 +1,15 @@
+function divisorSum(n: number): number {
+    let answer = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if(n % i === 0) {
+            answer += i;
+        }
+    }
+
+    return answer;
+}
+
+console.log(divisorSum(10));
+console.log(divisorSum(6));
+
