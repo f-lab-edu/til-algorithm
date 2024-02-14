aec764f90b877a12cfbb3f82c1b00076654102d0
김다인 <dbslzhs1218@naver.com>
2024-02-15 02:47:01 +0900

    feat: 240215_Lv1_x만큼 간격이 있는 n개의 숫자

---
+++ day1/Lv1_x만큼간격이있는n개의숫자.ts
@@ -0,0 +1,17 @@
+// 정수 x와 자연수 n
+// x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴
+// => x의 배수 n개를 리턴하면 됨
+
+function multipleArray(x: number, n: number): number[] {
+    let answer: number[] = [];
+    let num = 0;
+
+    while(answer.length < n) {
+        num += x;
+        answer.push(num);
+    } 
+
+    return answer;
+}
+
+console.log(multipleArray(2,5));
