e3a1387634dea80670428b97cded9b6ba1c76232
김다인 <dbslzhs1218@naver.com>
2024-02-14 23:39:50 +0900

    feat: 240214_Lv0_짝수의 합

---
+++ day1/Lv0_짝수의합.ts
@@ -0,0 +1,17 @@
+// n 이하의 짝수의 합 return 
+
+function evenSum(n: number): number {
+    let answer = 0;
+    let num = 0;
+
+    for(let num = 0; num <= n; num++) {
+        if(num % 2 === 0) {
+            answer += num;
+        }
+    }
+
+    return answer;
+}
+
+console.log(evenSum(20));
+console.log(evenSum(17));
