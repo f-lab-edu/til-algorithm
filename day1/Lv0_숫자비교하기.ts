5ec8e1baf34c037598dd2f6068616685cee58bc9
김다인 <dbslzhs1218@naver.com>
2024-02-14 23:02:45 +0900

    feat: 240214_Lv0_숫자 비교하기

---
+++ day1/Lv0_숫자비교하기.ts
@@ -0,0 +1,14 @@
+function compare(num1: number, num2: number): number {
+    let answer = -1;
+
+    if(num1 === num2) {
+        answer = 1;
+
+        return answer;
+    }
+
+    return answer;
+}
+
+console.log(compare(2,3));
+console.log(compare(2,2));
