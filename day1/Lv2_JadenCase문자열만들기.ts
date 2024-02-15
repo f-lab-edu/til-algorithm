afd07c3bcaf690416fea435f14194f8deae4e54f
김다인 <dbslzhs1218@naver.com>
2024-02-15 18:11:11 +0900

    feat: 240215_Lv2_JadenCase문자열 만들기

---
+++ day1/Lv2_JadenCase문자열만들기.ts
@@ -0,0 +1,26 @@
+//JadenCase란 모든 단어의 첫 문자가 대문자, 그 외의 알파벳은 소문자인 문자열 
+//문자열 s를 JadenCase로 바꾼 문자열을 리턴
+
+function jadenCase(s: string): string {
+    let strArray: string[] = [];
+    let answer: string = '';
+    let answerArray: string[] = [];
+
+    strArray = s.split(' ');
+
+    strArray.forEach((word) => {
+        let first = word.charAt(0).toUpperCase();
+        let other = word.slice(1).toLowerCase();
+
+        word = first + other;
+        
+        answerArray.push(word);
+    });
+
+    answer = answerArray.join(' ');
+
+    return answer;
+}
+
+console.log(jadenCase("3people unFollowed me"));
+console.log(jadenCase("for the last week"));
