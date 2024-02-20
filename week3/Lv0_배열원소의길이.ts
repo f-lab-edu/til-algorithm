8e3fea38ed7202b1eca4423f3ed39cc67da4d834
김다인 <dbslzhs1218@naver.com>
2024-02-20 22:16:28 +0900

    feat: 240220_Lv0_배열 원소의 길이

---
+++ week3/Lv0_배열원소의길이.ts
@@ -0,0 +1,14 @@
+// strlist는 알파벳 소문자, 대문자, 특수문자로 구성
+// strlist 각 원소의 길이를 담은 배열을 return
+import _ from 'lodash';
+
+function strLenght(strlist: string[]): number[] {
+  return strlist.map((str) => str.length);
+}
+
+function lodash_strLenght(strlist: string[]): number[] {
+  return _.map(strlist, (str) => str.length);
+}
+
+console.log(strLenght(['asc', 'Edfd', '#fD%^^']));
+console.log(lodash_strLenght(['asc', 'Edfd', '#fD%^^']));
