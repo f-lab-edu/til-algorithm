f85ea749bdb81e722531f1dab8f59fa9643090f5
김다인 <dbslzhs1218@naver.com>
2024-02-21 23:42:48 +0900

    feat: 240221_Lv1_정수 제곱근 판별

---
+++ week3/Lv1_정수제곱근판별.ts
@@ -0,0 +1,8 @@
+// n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴,
+// n이 양의 정수 x의 제곱이 아니라면 -1을 리턴
+function squarRoot(n: number): number {
+    return Number.isInteger(Math.sqrt(n)) ? Math.pow(Math.sqrt(n) + 1, 2) : -1;
+}
+
+console.log(squarRoot(25));
+console.log(squarRoot(12));
