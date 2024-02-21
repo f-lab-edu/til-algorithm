2d9211131d8507317b3f5cfa545cb8d4c5a9238e
김다인 <dbslzhs1218@naver.com>
2024-02-22 00:00:16 +0900

    feat: 240221_Lv2_올바른 괄호

---
+++ week3/Lv2_올바른괄호.ts
@@ -0,0 +1,25 @@
+// 올바른 괄호 = '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 함
+// '(' 또는 ')' 로만 이루어진 문자열 s
+// 문자열 s가 올바른 괄호이면 true를 return, 올바르지 않은 괄호이면 false를 return
+
+// 열린 괄호 수 = 닫힌 괄호 수
+// 맨 뒤는 닫힌 괄호가 맨 앞은 열린 괄호가 있어야함
+// abaabb()(()) ababab()()() aaabbb((()))  (()()) (())()
+// abbaab ())(()  )(()()
+function correctBracket(s: string): boolean {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    stack.push(s[i]);
+
+    if (stack[stack.length - 1] === ')' && stack[stack.length - 2] === '(') {
+      stack.pop();
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0 ? true : false;
+}
+
+console.log(correctBracket('((()))'));
+console.log(correctBracket(')()()('));
