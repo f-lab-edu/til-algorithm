14660555d3b6551c6f8d7d6d4ca251de62dbf895
김다인 <dbslzhs1218@naver.com>
2024-02-25 23:37:52 +0900

    refactor: 삼항연산자 사용

--- week2/Lv0_숫자비교하기.ts
+++ week2/Lv0_숫자비교하기.ts
@@ -1,7 +1,12 @@
-function compare(num1: number, num2: number): number {
-  if (num1 === num2) return 1;
+// function compare(num1: number, num2: number): number {
+//   if (num1 === num2) return 1;
 
-  return -1;
+//   return -1;
+// }
+
+function compare(num1: number, num2: number): number {
+
+  return num1 === num2 ? 1 : -1;
 }
 
 console.log(compare(2, 3));
