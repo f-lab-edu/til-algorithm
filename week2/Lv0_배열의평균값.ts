4f3e711c57b8bc270479ab54d184b929dd50907e
김다인 <dbslzhs1218@naver.com>
2024-02-25 23:36:51 +0900

    refactor: lodash 사용

--- week2/Lv0_배열의평균값.ts
+++ week2/Lv0_배열의평균값.ts
@@ -1,11 +1,25 @@
+// function averageOfArray(numbers: number[]): number {
+//   let average = 0;
+
+//   numbers.forEach((num) => {
+//     average += num;
+//   });
+
+//   return average / numbers.length;
+// }
+
+// function averageOfArray(numbers: number[]): number {
+//   const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+
+//   return sum / numbers.length;
+// }
+
+import _ from "lodash";
+
 function averageOfArray(numbers: number[]): number {
-  let average = 0;
+  const sum = _.sum(numbers);
 
-  numbers.forEach((num) => {
-    average += num;
-  });
-
-  return average / numbers.length;
+  return sum / numbers.length;
 }
 
 console.log(averageOfArray([2, 6, 7, 8, 12]));
