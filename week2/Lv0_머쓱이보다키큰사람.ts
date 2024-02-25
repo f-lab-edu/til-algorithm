0a2dedf4402837113abdc4ab477b6928eb983a72
김다인 <dbslzhs1218@naver.com>
2024-02-25 23:32:25 +0900

    refactor: let 사용 수정
    - let 사용을 지양하기 위해 forEach()말고 반환값을 주는 filter()로 변경

--- week2/Lv0_머쓱이보다키큰사람.ts
+++ week2/Lv0_머쓱이보다키큰사람.ts
@@ -2,16 +2,20 @@
 // array:반 친구들 키 배열, height: 머쓱이 키
 // 머쓱이보다 키 큰 사람 수 return
 
+// function numberOfTallPeople(array: number[], height: number): number {
+//   let tallNum = 0;
+
+//   array.forEach((num) => {
+//     if (height < num) {
+//       tallNum += 1;
+//     }
+//   });
+
+//   return tallNum;
+// }
+
 function numberOfTallPeople(array: number[], height: number): number {
-  let tallNum = 0;
-
-  array.forEach((num) => {
-    if (height < num) {
-      tallNum += 1;
-    }
-  });
-
-  return tallNum;
+  return array.filter((element) => element > height).length;
 }
 
-console.log(numberOfTallPeople([167, 172, 180], 175));
+console.log(numberOfTallPeople([167, 172, 178, 180], 175));
