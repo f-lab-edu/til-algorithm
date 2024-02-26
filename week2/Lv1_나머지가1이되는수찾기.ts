5d3c5811918672c89d88ca61f0196f11ecb9c2ad
김다인 <dbslzhs1218@naver.com>
2024-02-26 20:19:56 +0900

    refactor: for문으로 변경

--- week2/Lv1_나머지가1이되는수찾기.ts
+++ week2/Lv1_나머지가1이되는수찾기.ts
@@ -1,10 +1,25 @@
 // n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return
+// 3 ≤ n ≤ 1,000,000
+
+// function remainderIs1(n: number): number {
+//   let x = 1;
+
+//   while (x++) {
+  
+//     if (n % x === 1) {
+//       return x;
+//     }
+//   }
+
+//   return x;
+// }
 
 function remainderIs1(n: number): number {
   let x = 1;
 
-  while (x++) {
-    if (n % x === 1) {
+  for (let i = 1; i < n; i++) {
+    if (n % i === 1) {
+      x = i;
       return x;
     }
   }
@@ -12,5 +27,9 @@ function remainderIs1(n: number): number {
   return x;
 }
 
-console.log(remainderIs1(1));
+console.log(remainderIs1(3));
+console.log(remainderIs1(4));
+console.log(remainderIs1(5));
+console.log(remainderIs1(6));
+console.log(remainderIs1(7));
 console.log(remainderIs1(10));
