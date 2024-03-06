0e4183e67dfa85f671ffdc0c5c972625ef24106a
김다인 <dbslzhs1218@naver.com>
2024-03-06 20:43:31 +0900

    refactor: 올바른 괄호 숙제 구현

--- week3/Lv2_올바른괄호.ts
+++ week3/Lv2_올바른괄호.ts
@@ -5,15 +5,37 @@
 function correctBracket(s: string): boolean {
   const stack = [];
 
-  for (let i = 0; i < s.length; i++) {
-    stack.push(s[i]);
+  // for (let i = 0; i < s.length; i++) {
+  //   stack.push(s[i]);
 
-    if (stack[stack.length - 1] === ")" && stack[stack.length - 2] === "(") {
-      stack.pop();
-      stack.pop();
+  //   if (stack[stack.length - 1] === ")" && stack[stack.length - 2] === "(") {
+  //     stack.pop();
+  //     stack.pop();
+  //   }
+  // }
+
+  // return stack.length === 0;
+
+  // 해보기
+  for (const i of s) {
+    if (i === "(") {
+      // TODO:
+      stack.push(i);
+    } else {
+      if(stack.length === 0) {
+        return false;
+      }
+      // TODO:
+      if (stack.at(-1) === "(") {
+        stack.pop();
+      }
     }
-  }
 
+    // 괄호를 `stack`에 하나씩 넣으면서
+    // 열린 괄호 다음에 닫힌 괄호가 있으면
+    // 그 두 괄호를 빼준다.
+  }
+  // 결과적으로 `stack`의 길이가
   return stack.length === 0;
 }
 
