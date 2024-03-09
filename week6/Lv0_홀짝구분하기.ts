dc7301a4c140c9cad56bba43ccc979e0e6b15d45
김다인 <dbslzhs1218@naver.com>
2024-03-09 23:27:11 +0900

    feat: Lv0_홀짝 구분하기

---
+++ week6/Lv0_홀짝구분하기.ts
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181944
+// 자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 홀수이면 "n is odd"를 출력하는 코드를 작성
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input: number;
+
+rl.on("line", function (line: string) {
+  input = Number(line);
+  rl.close();
+}).on("close", function () {
+  const n = input % 2 === 0 ? input + ' is even' : input + ' is odd'
+  console.log(n);
+});
