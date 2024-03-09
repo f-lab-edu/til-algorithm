4019f527b9751d3cd54a1beef0ff07aa843489c7
김다인 <dbslzhs1218@naver.com>
2024-03-09 23:24:54 +0900

    feat: Lv0_문자열로 변환

---
+++ week6/Lv0_문자열로변환.ts
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181845
+// 정수 n이 주어질 때, n을 문자열로 변환하여 return
+
+function changeStgring(n: number): string {
+  return String(n);
+}
+
+console.log(typeof(changeStgring(34)));
