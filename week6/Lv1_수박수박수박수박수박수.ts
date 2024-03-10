77b75461f07b0f08e06fbc48ce82346461c26cdc
김다인 <dbslzhs1218@naver.com>
2024-03-11 03:16:31 +0900

    feat: Lv1_수박수박수박수박수박수

---
+++ week6/Lv1_수박수박수박수박수박수.ts
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12922
+// 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴
+// 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴
+
+function waterMelon(n: number): string {
+  const word = "수박";
+
+  if (n % 2 === 0) {
+    return word.repeat(n / 2);
+  }
+
+  return word.repeat(n / 2) + word.at(0);
+}
+
+console.log(waterMelon(5));
+console.log(waterMelon(3));
+console.log(waterMelon(4));
