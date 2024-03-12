53fb07ce3cdcb72743517414f860824616ff895d
김다인 <dbslzhs1218@naver.com>
2024-03-12 19:08:34 +0900

    refactor: 최대공약수 유클리드 호제법으로 구현

--- week6/Lv1_최대공약수와최소공배수.ts
+++ week6/Lv1_최대공약수와최소공배수.ts
@@ -2,6 +2,7 @@
 // 두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환
 // 리턴 값 => [최대공약수,최소공배수]
 
+/** 
 function GCDLCM(n: number, m: number): number[] {
   const max = Math.max(n, m);
   const min = Math.min(n, m);
@@ -18,6 +19,24 @@ function GCDLCM(n: number, m: number): number[] {
   const LCM = max % min === 0 ? max : (max * min) / GCD;
 
   return [GCD, LCM];
+}
+*/
+
+function GCDLCM(n: number, m: number): number[] {
+  const max = Math.max(n, m);
+  const min = Math.min(n, m);
+  const GCD = gcd(max, min);
+
+  const LCM = max % min === 0 ? max : (max * min) / GCD;
+
+  return [GCD, LCM];
+}
+
+function gcd(a: number, b: number): number {
+  if (a % b === 0) {
+    return b;
+  }
+  return gcd(b, a % b);
 }
 
 console.log(GCDLCM(2, 5));
