ec69c6d3a925749630698b3a338829184573b0ef
김다인 <dbslzhs1218@naver.com>
2024-03-13 16:04:08 +0900

    refactor: lodash 함수 사용

--- week6/Lv1_행렬의덧셈.ts
+++ week6/Lv1_행렬의덧셈.ts
@@ -2,9 +2,10 @@
 // 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과
 // 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환
 
+import _ from "lodash";
 function addRowColumn(arr1: number[][], arr2: number[][]): number[][] {
   return arr1.map((numArr, arrIdx) =>
-    numArr.map((num, idx) => num + arr2[arrIdx][idx])
+    _.zipWith(numArr, arr2[arrIdx], (a, b) => a + b)
   );
 }
 
