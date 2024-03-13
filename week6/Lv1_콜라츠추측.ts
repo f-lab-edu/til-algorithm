b3326fd8fe1db843c82f0d6b93fe857ec996b08d
김다인 <dbslzhs1218@naver.com>
2024-03-13 14:48:27 +0900

    refactor: 중복 제거

--- week6/Lv1_콜라츠추측.ts
+++ week6/Lv1_콜라츠추측.ts
@@ -15,11 +15,10 @@ function guess(num: number): number {
 
     if (num % 2 === 0) {
       num = num / 2;
-      count++;
     } else {
       num = num * 3 + 1;
-      count++;
     }
+    count++;
   }
 
   return count;
