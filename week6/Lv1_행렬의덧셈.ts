de7ceedaf3983f3185c71d23b7387c0f1f557d4b
김다인 <dbslzhs1218@naver.com>
2024-04-02 07:28:22 +0900

    refactor: _.zipWith만 사용

--- week6/Lv1_행렬의덧셈.ts
+++ week6/Lv1_행렬의덧셈.ts
@@ -4,8 +4,13 @@
 
 import _ from "lodash";
 function addRowColumn(arr1: number[][], arr2: number[][]): number[][] {
-  return arr1.map((numArr, arrIdx) =>
-    _.zipWith(numArr, arr2[arrIdx], (a, b) => a + b)
+  // return arr1.map((numArr, arrIdx) =>
+  //   _.zipWith(numArr, arr2[arrIdx], (a, b) => a + b)
+  // );
+  //console.log(_.zip(arr1, arr2));
+
+  return _.zipWith(arr1, arr2, (elementArr1, elementArr2) =>
+    _.zipWith(elementArr1, elementArr2, (num1, num2) => num1 + num2)
   );
 }
 
