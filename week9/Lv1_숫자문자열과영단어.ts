9a76e9701fe3f8961d8f0271f4482ea6b8f57b9b
김다인 <dbslzhs1218@naver.com>
2024-04-01 23:30:00 +0900

    style: 함수 이름 변경

--- week9/Lv1_숫자문자열과영단어.ts
+++ week9/Lv1_숫자문자열과영단어.ts
@@ -5,7 +5,7 @@
 // 1478 → "one4seveneight", 234567 → "23four5six7", 10203 → "1zerotwozero3"
 // 이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다. s가 의미하는 원래 숫자를 return
 
-function solution(s: string): number {
+function changeNumStr(s: string): number {
   const numArr = [
     "zero",
     "one",
@@ -28,5 +28,5 @@ function solution(s: string): number {
   return Number(s);
 }
 
-console.log(solution("one4seveneight"));
-console.log(solution("1zerotwozero3"))
+console.log(changeNumStr("one4seveneight"));
+console.log(changeNumStr("1zerotwozero3"))
