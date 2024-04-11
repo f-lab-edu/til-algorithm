4218fc15ef6ecb1bf95d00ce2a0cb007beb0f28d
김다인 <dbslzhs1218@naver.com>
2024-04-11 20:17:03 +0900

    feat: Lv1_피보나치 수

---
+++ week10/Lv1_피보나치수.ts
@@ -0,0 +1,23 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12945
+// 피보나치 수는 F(0) = 0, F(1) = 1일 때, 1 이상의 n에 대하여 F(n) = F(n-1) + F(n-2) 가 적용되는 수
+// F(2) = F(0) + F(1) = 0 + 1 = 1
+// F(3) = F(1) + F(2) = 1 + 1 = 2
+// F(4) = F(2) + F(3) = 1 + 2 = 3
+// F(5) = F(3) + F(4) = 2 + 3 = 5
+// 2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 return
+
+function fibonacci(n: number): number {
+  let n1 = 1;
+  let n2 = 1;
+
+  for (let i = 3; i <= n; i++) {
+    const n3 = n1 + n2;
+    n1 = n2;
+    n2 = n3;
+  }
+
+  return n2 % 1234567;
+}
+
+console.log(fibonacci(3));
+console.log(fibonacci(5));
