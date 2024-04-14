54056a2138cc4089c83f772881c0d5f7b9d6bbc0
김다인 <dbslzhs1218@naver.com>
2024-04-14 23:10:41 +0900

    style: 함수 이름 변경 및 주석 삭제

--- week10/Lv1_피보나치수.ts
+++ week10/Lv1_피보나치수.ts
@@ -6,13 +6,12 @@
 // F(5) = F(3) + F(4) = 2 + 3 = 5
 // 2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 return
 
-function fibonacci1(n: number): number {
+function divideFbonacci(n: number): number {
   let fibonacciN = 1;
 
   if (n >= 3) {
     const fibonacciArray = [...fibonacci(n)];
     fibonacciN = fibonacciArray[fibonacciArray.length - 1];
-    //console.log(fibonacci2);
   }
 
   return fibonacciN;
@@ -27,15 +26,14 @@ function* fibonacci(n: number): Iterable<number> {
     n1 = n2 % 1234567;
     n2 = n3 % 1234567;
 
-    //console.log(n2)
     yield n2;
   }
 }
 
-console.log(fibonacci1(3));
-console.log(fibonacci1(5));
-console.log(fibonacci1(8));
-console.log(fibonacci1(1000));
-console.log(fibonacci1(999));
-console.log(fibonacci1(1001));
-console.log(fibonacci1(1002));
+console.log(divideFbonacci(3));
+console.log(divideFbonacci(5));
+console.log(divideFbonacci(8));
+console.log(divideFbonacci(1000));
+console.log(divideFbonacci(999));
+console.log(divideFbonacci(1001));
+console.log(divideFbonacci(1002));
