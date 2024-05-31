86a80849b8c3dd16062164f83b9100cb016795b7
김다인 <dbslzhs1218@naver.com>
2024-05-31 23:36:32 +0900

    feat: Lv0_이어붙인 수

---
+++ week18/Lv0_이어붙인수.ts
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181928
+// 정수가 담긴 리스트 num_list가 주어집니다. num_list의 홀수만 순서대로 이어 붙인 수와 짝수만 순서대로 이어 붙인 수의 합을 return
+
+function solution(num_list: number[]): number {
+  const even = Number(num_list.filter((num) => num % 2 === 0).join(''));
+  const odd = Number(num_list.filter((num) => num % 2 !== 0).join(''));
+
+  return even + odd;
+}
