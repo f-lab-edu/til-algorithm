3f12f95d2b2c60a5036e7b619e8253d3d2ea5ba6
김다인 <dbslzhs1218@naver.com>
2024-05-31 23:37:17 +0900

    feat: Lv0_중복된 문자 제거

---
+++ week18/Lv0_중복된문자제거.ts
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120888
+// 문자열 my_string이 매개변수로 주어집니다. my_string에서 중복된 문자를 제거하고 하나의 문자만 남긴 문자열을 return
+
+function solution(my_string: string): string {
+
+  return [...new Set(my_string)].join('');
+}
+
+console.log(solution("We are the world"))
+console.log(solution("people"))
