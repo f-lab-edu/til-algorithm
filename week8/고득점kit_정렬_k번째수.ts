e71ec00b98cbfc6396b0a16a51644dcab30593a9
김다인 <dbslzhs1218@naver.com>
2024-06-05 06:44:02 +0900

    refactor: numArr 제거

--- week8/고득점kit_정렬_k번째수.ts
+++ week8/고득점kit_정렬_k번째수.ts
@@ -9,9 +9,7 @@
 
 import _ from "lodash";
 function commandsNumber(array: number[], commands: number[][]): number[] {
-  const commandsArr = commands.map((numArr) => {
-    const [i, j, k] = numArr;
-
+  const commandsArr = commands.map(([i, j, k]) => {
     const commandArr = _.sortBy(array.slice(i - 1, j));
     return commandArr[k - 1];
   });
