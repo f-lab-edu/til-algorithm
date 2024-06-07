ad68a88bd551812e76b521d7e3765a8a0338dde6
김다인 <dbslzhs1218@naver.com>
2024-06-07 09:25:57 +0900

    refactor: 공통적인 부분 일반화하기

--- week11/Lv0_개미군단.ts
+++ week11/Lv0_개미군단.ts
@@ -5,28 +5,22 @@
 // 예를 들어 체력 23의 여치를 사냥하려고 할 때, 일개미 23마리를 데리고 가도 되지만, 장군개미 네 마리와 병정개미 한 마리를 데리고 간다면 더 적은 병력으로 사냥할 수 있습니다.
 // 사냥감의 체력 hp가 매개변수로 주어질 때, 사냥감의 체력에 딱 맞게 최소한의 병력을 구성하려면 몇 마리의 개미가 필요한지를 return
 
-function antArmy(hp: number): number {
+function buildingAnAntArmy(hp: number): number {
   let count = 0;
+  const offensePower = [5, 3, 1];
   const antCounting = (ant: number) => {
     const antCount = Math.floor(hp / ant);
     count += antCount;
     hp -= antCount * ant;
   };
 
-  if (hp >= 5) {
-    antCounting(5);
+  for (const x of offensePower) {
+    if (hp >= x) {
+      antCounting(x);
+    }
   }
-
-  if (hp >= 3) {
-    antCounting(3);
-  }
-
-  if (hp >= 1) {
-    antCounting(1);
-  }
-
   return count;
 }
 
-console.log(antArmy(23));
-console.log(antArmy(999));
+console.log(buildingAnAntArmy(23));
+console.log(buildingAnAntArmy(999));
