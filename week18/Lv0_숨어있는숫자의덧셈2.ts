703c9c3d7004e9f8637e1b46498524d74d91c50d
김다인 <dbslzhs1218@naver.com>
2024-06-08 00:07:27 +0900

    refactor: lodash 함수 사용

--- week18/Lv0_숨어있는숫자의덧셈2.ts
+++ week18/Lv0_숨어있는숫자의덧셈2.ts
@@ -2,12 +2,14 @@
 // 문자열 my_string이 매개변수로 주어집니다. my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. my_string안의 자연수들의 합을 return
 
 import _ from "lodash";
-function sumOfHiddenNumber(my_string: string): number {
-  return _.sum(
-    my_string
-      .split(/\D+/)
-      .filter((num) => num !== "")
-      .map(Number)
-  );
+function solution(my_string: string): number {
+  // return _.sum(
+  //   my_string
+  //     .split(/\D+/)
+  //     .filter((num) => num !== "")
+  //     .map(Number)
+  // );
+  
+  return _.sum(_.compact(my_string.split(/\D+/)).map(Number));
 }
-console.log(sumOfHiddenNumber("aAb1B2cC34oOp"));
+console.log(solution("aAb1B2cC34oOp"));
