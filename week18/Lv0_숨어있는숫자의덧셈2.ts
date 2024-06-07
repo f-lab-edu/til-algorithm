01f987369ac3712284045e69705587b37a8bf4fe
김다인 <dbslzhs1218@naver.com>
2024-06-07 23:39:25 +0900

    feat: Lv0_숨어있는 숫자의 덧셈

---
+++ week18/Lv0_숨어있는숫자의덧셈2.ts
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120864
+// 문자열 my_string이 매개변수로 주어집니다. my_string은 소문자, 대문자, 자연수로만 구성되어있습니다. my_string안의 자연수들의 합을 return
+
+import _ from "lodash";
+function sumOfHiddenNumber(my_string: string): number {
+  return _.sum(
+    my_string
+      .split(/\D+/)
+      .filter((num) => num !== "")
+      .map(Number)
+  );
+}
+console.log(sumOfHiddenNumber("aAb1B2cC34oOp"));
