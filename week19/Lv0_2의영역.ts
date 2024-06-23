793157100a8d06dc5fae243904e010355ee9941d
김다인 <dbslzhs1218@naver.com>
2024-06-23 23:48:19 +0900

    feat: Lv0_2의 영역

---
+++ week19/Lv0_2의영역.ts
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181894
+
+// 정수 배열 arr가 주어집니다. 배열 안의 2가 모두 포함된 가장 작은 연속된 부분 배열을 return
+// 단, arr에 2가 없는 경우 [-1]을 return
+
+function solution(arr: number[]): number[] {
+  const NUMBER = 2;
+  const start = arr.indexOf(NUMBER);
+  const end = arr.lastIndexOf(NUMBER);
+
+  return start === -1 || end === -1 ? [-1] : arr.slice(start, end + 1);
+}
+
+console.log(solution([1, 2, 1, 4, 5, 2, 9]));
+console.log(solution([1, 9]));
+console.log(solution([1, 2, 1, 2, 5, 2, 9]));
+console.log(solution([1, 2, 1]));
