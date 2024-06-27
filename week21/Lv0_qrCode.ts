8fac0aa5df9ae1d393d0e3ec9efd3d364b5b0548
김다인 <dbslzhs1218@naver.com>
2024-06-27 23:59:08 +0900

    feat: Lv0_qr code

---
+++ week21/Lv0_qrCode.ts
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181903
+
+// 두 정수 q, r과 문자열 code가 주어질 때, code의 각 인덱스를 q로 나누었을 때 나머지가 r인 위치의 문자를 앞에서부터 순서대로 이어 붙인 문자열을 return
+
+function solution(q: number, r: number, code: string): string {
+  return [...code].filter((_, idx) => idx % q === r).join("");
+}
+
+console.log(solution(3, 1, "qjnwezgrpirldywt"));
