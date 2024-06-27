35381240db27e33d2cd08759b64aff134dccf23e
김다인 <dbslzhs1218@naver.com>
2024-06-27 23:59:41 +0900

    feat: Lv0_문자 개수 세기

---
+++ week21/Lv0_문자개수세기.ts
@@ -0,0 +1,25 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181902
+
+// 알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때,
+// my_string에서 'A'의 개수, my_string에서 'B'의 개수,..., my_string에서 'Z'의 개수, my_string에서 'a'의 개수, my_string에서 'b'의 개수,...,
+// my_string에서 'z'의 개수를 순서대로 담은 길이 52의 정수 배열을 return
+
+// 알파벳 배열 생성
+// my_string을 돌며
+import _ from "lodash";
+function solution(my_string: string): number[] {
+  const xs = _.range(65, 91).concat(_.range(97, 123));
+  const map = new Map();
+
+  for (const x of xs) {
+    map.set(String.fromCharCode(x), 0);
+  }
+
+  for (const str of my_string) {
+    map.set(str, map.get(str) + 1);
+  }
+
+  return [...map.values()];
+}
+
+console.log(solution("Programmers"));
