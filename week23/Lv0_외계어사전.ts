4d4fd20dd18ab725304c883ff83bd394b9c16039
김다인 <dbslzhs1218@naver.com>
2024-07-14 23:53:22 +0900

    feat: Lv0_외계어 사전

---
+++ week23/Lv0_외계어사전.ts
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120869
+
+// PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다.
+// 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다.
+// spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return
+
+// spell를 모두 사용한 단어가 dic에 존재하는지 확인
+
+function solution(spell: string[], dic: string[]): number {
+  return dic.some((word) => spell.every((str) => word.includes(str))) ? 1 : 2;
+}
+
+// 존재할 때
+console.log(solution(["z", "d", "x"], ["def", "dww", "dzx", "loveaw"]));
+console.log(solution(["q", "e", "n", "p"], ["enqpo", "loveaw"]));
+
+// 아닐 때
+console.log(solution(["p", "o", "s"], ["sod", "eocd", "qpixm", "adio", "soo"]));
